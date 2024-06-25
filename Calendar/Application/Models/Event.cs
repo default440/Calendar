a6813ff03afc4dcd1c59a4a1579f37790aadc655
ilia.bylyba <sksf.bylyba@gmail.com>
2024-06-25 22:41:32 +0700

    Первый прототип для модели события

---
+++ Calendar/Application/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿namespace Calendar.Application.Models
+{
+    internal record Event : IEvent
+    {
+        public Guid Id { get; }
+        public string Name { get; set; }
+        public string? Description { get; set; } = null;
+        public DateTimeOffset? Date { get; set; } = null;
+        public IReadOnlyList<IEvent> childEvents { get; set; } = Array.Empty<IEvent>();
+
+        public Event(string name)
+        {
+            Name = name;
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ Calendar/Application/Models/IEvent.cs
@@ -0,0 +1,15 @@
+﻿namespace Calendar.Application.Models
+{
+    internal interface IEvent
+    {
+        public Guid Id { get; }
+
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTimeOffset? Date { get; set; }
+
+        public IReadOnlyList<IEvent> childEvents { get; set; }
+    }
+}
